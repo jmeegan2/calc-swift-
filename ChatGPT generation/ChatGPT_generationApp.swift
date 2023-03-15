dd5c5707120b77ca59097edfe6a806db0bc0f79c
jmeegan2 <jmeegan8@gmail.com>
2023-03-15 19:34:55 -0400

    init commit

---
+++ ChatGPT generation/ChatGPT_generationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_generationApp.swift
+//  ChatGPT generation
+//
+//  Created by James Meegan on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_generationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPT generation/ContentView.swift
@@ -0,0 +1,104 @@
+//
+//  ContentView.swift
+//  ChatGPT generation
+//
+//  Created by James Meegan on 3/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let buttons = [
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "−"],
+        ["1", "2", "3", "+"],
+        [".", "0", "C", "="]
+    ]
+    
+    @State var currentNumber: String = ""
+    @State var storedNumber: Double = 0
+    @State var storedOperation: String = ""
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Spacer()
+            HStack(spacing: 12) {
+                Text(currentNumber)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .lineLimit(1)
+                Spacer()
+            }
+            ForEach(buttons, id: \.self) { row in
+                HStack(spacing: 12) {
+                    ForEach(row, id: \.self) { button in
+                        Button(action: {
+                            self.buttonTapped(button)
+                        }) {
+                            Text(button)
+                                .font(.headline)
+                                .frame(width: 70, height: 70)
+                                .foregroundColor(.white)
+                                .background(Color.gray)
+                                .cornerRadius(35)
+                        }
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+    
+    func buttonTapped(_ button: String) {
+        if let number = Double(button) {
+            if currentNumber == "0" {
+                currentNumber = button
+            } else {
+                currentNumber += button
+            }
+        } else if button == "." {
+            if !currentNumber.contains(".") {
+                currentNumber += "."
+            }
+        } else if button == "C" {
+            currentNumber = "0"
+            storedNumber = 0
+            storedOperation = ""
+        } else if let operation = button.first {
+            if storedOperation.isEmpty {
+                storedNumber = Double(currentNumber) ?? 0
+                currentNumber = ""
+            } else {
+                performCalculation()
+            }
+            storedOperation = String(operation)
+        } else if button == "=" {
+            performCalculation()
+        }
+    }
+    
+    func performCalculation() {
+        if let operation = storedOperation.first {
+            switch operation {
+            case "+":
+                storedNumber += Double(currentNumber) ?? 0
+            case "−":
+                storedNumber -= Double(currentNumber) ?? 0
+            case "×":
+                storedNumber *= Double(currentNumber) ?? 0
+            case "÷":
+                storedNumber /= Double(currentNumber) ?? 1
+            default:
+                break
+            }
+        }
+        currentNumber = String(storedNumber)
+        storedOperation = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
